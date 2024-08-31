5dde2bc9cde8973e53a4db21ad500704b978316f
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-08-31 14:08:14 -0700

    more stlyes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,14 +4,14 @@ import './Footer.css'
 export default function Footer() {
   return (
     <div className='footerMainBox'>
-      <p>
+      <p className='footerParagraphBoxes'>
+      The Slurping Slug copyright 2024
+      </p>
+      <p className='footerParagraphBoxes'>
       contact trevor: (#$@) %$%-&^*@
       </p>
-      <p>
-        The Slurping Slug copyright 2024
-      </p>
-      <p>
-        webbage: <a href="https://hightower-resume.vercel.app/">hightowerbuilds</a>
+      <p className='footerParagraphBoxes'>
+        webbage: <a href="https://hightower-resume.vercel.app/">hightower builds</a>
       </p>
     </div>
   )
