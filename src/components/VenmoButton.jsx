902a93b1b31b4b7d91811eeaa1179582f03a7cf1
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-08-31 16:25:18 -0700

    styles

--- src/components/VenmoButton.jsx
+++ src/components/VenmoButton.jsx
@@ -2,18 +2,12 @@
 import payWithVenmo from '../images/payWithVenmo.jpg'
 
 export default function VenmoButton() {
-  const venmoUsername = 'Trevor-Robison-1';
 
-  const handleVenmoClick = () => {
-    window.location.href = `https://venmo.com/${venmoUsername}`;
-  };
 
   return (
-    <div >
-      <button onClick={handleVenmoClick} style={{ backgroundColor: '#000000', color: 'seagreen', fontFamily: 'courier', fontSize: 30}}>
-      <img src={payWithVenmo} alt="" />
-      </button>
-    </div>
+    <a href='https://venmo.com/Trevor-Robison-1' target="_blank">
+      <img src={payWithVenmo} alt="PAY WITH VENMO!" />
+    </a>
   );
 }
 
