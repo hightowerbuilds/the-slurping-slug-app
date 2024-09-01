0c49f6af6779a0aeca3713d9722ffb818dd9325d
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-09-01 09:24:19 -0700

    bringing in the message board

--- src/pages/CommentsPage.jsx
+++ src/pages/CommentsPage.jsx
@@ -1,19 +1,49 @@
 
 import './CommentsPage.css'
+import { useState } from 'react'
 import DashboardNavBar from "../components/DashboardNavBar"
 import Footer from '../components/Footer'
+import messageLogo from '../images/browserImages/messageBoardLogo.jpg'
 
 
 export default function CommentsPage() {
+
+  const [ newMessage, setNewMessage ] = useState('')
+  const [ messageList, setMessageList] = useState([])
+
+
+  const messageHandler = (e) => {
+    setNewMessage(e.target.value)
+  }
+
+  const listHandler = () => {
+    setMessageList([...messageList, newMessage])
+  }
+
+
   return (
     <div style={{
         width: '100%',
         height: '100vh',
         backgroundColor: '#000000',
         color: 'white'
-    }}>
+    }}> 
      <DashboardNavBar />
-        message booard
+        
+    <div>
+      <img src={messageLogo} />
+    </div>
+
+    <div>
+    <input type="text" value={newMessage} onChange={messageHandler}/>
+    <button onClick={listHandler} >send</button>
+    </div>
+
+    <div>
+      {[...messageList].reverse().map((message, index) => (<p key={index}>{message}</p>))}
+    </div>
+
+
     <br />
         <Footer />
     </div>
