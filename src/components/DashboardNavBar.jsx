662256ecc366f6d44f447209b83c3b8ca597245a
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-09-04 21:28:44 -0700

    mobile sstyles

--- src/components/DashboardNavBar.jsx
+++ src/components/DashboardNavBar.jsx
@@ -7,6 +7,13 @@ import musicLogo from '../images/browserImages/musicLogo.jpg'
 import sluglickerLogo from '../images/browserImages/sluglickerLogo.jpg'
 import noodlesLogo from '../images/browserImages/noodlesLogo.jpg'
 import peepsLogo from '../images/browserImages/peepsLogo.jpg'
+import comicsLogoMobile from '../images/mobileImages/comicsLogoMobile.jpg'
+import musicLogoMobile from '../images/mobileImages/musicLogoMobile.jpg'
+import noodlesLogoMobile from '../images/mobileImages/noodlesLogoMobile.jpg'
+import peepsLogoMobile from '../images/mobileImages/peepsLogoMobile.jpg'
+
+
+
 
 export default function DashboardNavBar() {
   return (
@@ -14,26 +21,35 @@ export default function DashboardNavBar() {
     <div className="homePageExteriorNavBar">
         <div className="homePageMiddleNavBar">
             <div className="homePageInteriorNavBar">
+                <div className='slugBoxMobile'>
                 <NavLink style={{ width: '200px', display: 'flex', flexWrap: 'wrap', justifyContent: 'center'}} to='/'>
                     <img src={sluglickerLogo}/><br />
                     <img src={slugs}/>
                 </NavLink>
-
+                </div>
+               
+                <div className='navBarButtonBoxDesktop'>
                 <NavLink  to="/gallery" > 
-                    <img src={musicLogo} />
+                    <img className='musicLogoMobile' src={musicLogoMobile}/>
+                    <img className='musicLogoDesktop' src={musicLogo} />
                 </NavLink>
 
                 <NavLink to="/comics" >
-                    <img src={comicsLogo} />
+                    <img className='comicsLogoMobile' src={comicsLogoMobile} />
+                    <img className='comicsLogoDesktop' src={comicsLogo} />
                 </NavLink>
 
                 <NavLink to="/store" >
-                    <img src={noodlesLogo} />
+                    <img className='noodlesLogoMobile' src={noodlesLogoMobile}/>
+                    <img className='noodlesLogoDesktop' src={noodlesLogo} />
                 </NavLink>
 
                 <NavLink to="/comments" >
-                    <img src={peepsLogo} />
+                    <img className='peepsLogoMobile' src={peepsLogoMobile}/>
+                    <img className='peepsLogoDesktop' src={peepsLogo} />
                 </NavLink>
+                </div>
+           
             </div>
         </div>
     </div>
